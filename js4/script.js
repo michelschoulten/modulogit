eb133254a9ddcd2892d1ea80416a8f3e1b77ae69
michel <michelschoulten@gmail.com>
2021-05-05 13:33:34 -0300

    Script de Calculadora

--- js4/script.js
+++ js4/script.js
@@ -1,2 +1,8 @@
-var x = 10 + 20;
-alert(x);
+function somar(){
+    var campo1 = parseInt(document.getElementById("campo1").value);
+    var campo2 = parseInt(document.getElementById("campo2").value);
+
+ var soma = campo1 * campo2;
+ alert(soma);
+
+}
