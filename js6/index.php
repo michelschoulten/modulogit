0dd138435c702b18c534463b27eda9f6a2064614
michel <michelschoulten@gmail.com>
2021-05-12 23:10:12 -0300

    onkeyboard

---
+++ js6/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="scripts.js" >
+    </script>
+<title>Keyboardevent</title>
+</head>
+<body>
+
+<div>
+<input type="text" onkeypress="apertouTecla(event)"/>
+</div>
+    
+</body>
+</html>
