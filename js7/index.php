bf8383df4f5b06284e7707ebecba85993395acdc
michel <michelschoulten@gmail.com>
2021-05-13 00:10:19 -0300

    Jogo de bolas

---
+++ js7/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" />
+    <title>Javascript</title>
+    <script type="text/javascript" src="script.js">
+    </script>
+    
+</head>
+<body onload="iniciar()" id="principal">
+    <div class="topo">
+    <div class="container">
+    </div>
+    <div class="pontos" id="pontos">0</div>
+    </div>
+    <div class="botoes">
+        <button onclick="parar()">Parar!</button>
+        <button onclick="iniciar()">Continuar!</button>
+        <button onclick="limpar()">Limpar!</button>
+    </div>
+</body>
+</html>
